bc5b24968960a1782f7b0fe977a371aabba4c156
Mirele Fernandes <mirele.mrf@gmail.com>
2023-05-29 14:24:53 -0300

    aulas 1-2

---
+++ src/services/LoginService/LoginService.ts
@@ -0,0 +1,8 @@
+import { AxiosError } from "axios";
+
+import Api from "../../clients/api/Api";
+
+import { LoginParams, LoginValues } from "../../domain/login";
+import { RequestError } from "../../domain/request";
+
+const;
